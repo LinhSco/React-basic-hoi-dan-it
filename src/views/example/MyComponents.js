ff99c259c61a337434493cc8f4854fcf8426bfc2
Linh Sco <mydreamlinhsco@gmail.com>
2023-03-14 23:09:11 +0700

    video 7

--- src/views/example/MyComponents.js
+++ src/views/example/MyComponents.js
@@ -4,19 +4,24 @@ class MyComponent extends React.Component {
     state = {
         name: "eric",
         channel: "Hoi Dan IT"
+    };
+    handleClickButton = () => {
+        alert('click me');
     }
     render() {
         let name = "eric";
 
         return (
             <>
-                < div >
+                < div className="one" >
                     {console.log("Name: ", name)}
                     Chao Linh Sco Components. Name: {this.state.channel}
                 </div >
-                < div >
-                    {console.log("Name: ", name)}
+                < div className="two" >
                     Chao Linh Sco Components. Name: {name}
+                </div >
+                < div className="three" >
+                    <button onClick={() => this.handleClickButton()}>Click Me</button>
                 </div >
             </>
         )
